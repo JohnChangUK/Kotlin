3b5e637a88b2f3926c981e2b9adbe8138d889ef4
John Chang <johnchang9094@gmail.com>
2017-05-22 13:22:03 +0100

    Added Lambdas in Kotlin

--- src/main.kt
+++ src/main.kt
@@ -1,3 +1,23 @@
+import com.rsk.Person
+
 fun main(args: Array<String>) {
-    println("Hello, World wow I need a way to break this line")
+    println("Hello, World, printing to the console")
+
+    val john = Person("John")
+
+
+    println("Name is ${john.Name}")
+
+    john.Name = "John C Makers"
+
+    println("Name is now ${john.Name}")
+
+    john.display()
+            // :: means function reference, like Java
+    john.displayWithLambda(::printName)
+
+}
+
+fun printName(name: String) {
+    println(name)
 }
